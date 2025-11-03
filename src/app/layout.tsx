95638bc902787ae1ae31b495944fc2b779c2049d
우디쌤의영화입시학원Woodie Film Academy <begnino_film@naver.com>
2025-11-03 10:59:35 +0900

    Add Vercel Speed Insights to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google"
 
 import ClientClockInitializer from "@/components/ClientClockInitializer"
 import DateUtil from "@/lib/date-util"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import "./globals.css"
 
@@ -35,6 +36,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} bg-slate-50 antialiased`}>
         <ClientClockInitializer serverNow={serverNow} />
         {children}
+        <SpeedInsights />
       </body>
     </html>
   )
