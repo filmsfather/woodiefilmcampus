df05106c212ae2938315ce77695dae97d02f4f5e
우디쌤의영화입시학원Woodie Film Academy <begnino_film@naver.com>
2025-12-06 20:43:36 +0900

    fix: support youtube shorts urls

--- src/lib/lectures.ts
+++ src/lib/lectures.ts
@@ -64,7 +64,8 @@ export async function deleteLecture(supabase: SupabaseClient, id: string) {
 }
 
 export function getYoutubeVideoId(url: string) {
-    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/
+    if (!url) return null
+    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=|shorts\/)([^#&?]*).*/
     const match = url.match(regExp)
     return (match && match[2].length === 11) ? match[2] : null
 }
