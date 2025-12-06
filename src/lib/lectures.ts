b44d8c8e8c7ce0fd925c2ec248aeb5e41fcae930
우디쌤의영화입시학원Woodie Film Academy <begnino_film@naver.com>
2025-12-06 20:54:13 +0900

    fix: support youtube live urls

--- src/lib/lectures.ts
+++ src/lib/lectures.ts
@@ -65,7 +65,7 @@ export async function deleteLecture(supabase: SupabaseClient, id: string) {
 
 export function getYoutubeVideoId(url: string) {
     if (!url) return null
-    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=|shorts\/)([^#&?]*).*/
+    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=|shorts\/|live\/)([^#&?]*).*/
     const match = url.match(regExp)
     return (match && match[2].length === 11) ? match[2] : null
 }
